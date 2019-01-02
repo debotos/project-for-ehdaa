ad1d33fc248e531ed9dc0c8a5b99169ec9aa9c69
Debotos Das <debotosdas@gmail.com>
2019-01-02 13:51:27 +0600

    initial structure

---
+++ src/hoc/layout.js
@@ -0,0 +1,266 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import Hidden from '@material-ui/core/Hidden';
+import IconButton from '@material-ui/core/IconButton';
+import InputBase from '@material-ui/core/InputBase';
+import { fade } from '@material-ui/core/styles/colorManipulator';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Typography from '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+
+import SearchIcon from '@material-ui/icons/Search';
+import MenuIcon from '@material-ui/icons/Menu';
+import AddBox from '@material-ui/icons/AddBox';
+import Edit from '@material-ui/icons/Edit';
+import Details from '@material-ui/icons/Details';
+import Filter from '@material-ui/icons/Filter1Sharp';
+
+const drawerWidth = 240;
+const optionsTextArray = ['Add New Account', 'Edit', 'Details', 'Filter'];
+const optionsIconArray = [<AddBox />, <Edit />, <Details />, <Filter />];
+
+const styles = theme => ({
+	root: {
+		display: 'flex'
+	},
+	grow: {
+		flexGrow: 1
+	},
+	drawer: {
+		[theme.breakpoints.up('sm')]: {
+			width: drawerWidth,
+			flexShrink: 0
+		}
+	},
+	appBar: {
+		marginLeft: drawerWidth,
+		[theme.breakpoints.up('sm')]: {
+			width: `calc(100% - ${drawerWidth}px)`
+		}
+	},
+	menuButton: {
+		marginRight: 20,
+		[theme.breakpoints.up('sm')]: {
+			display: 'none'
+		}
+	},
+	toolbar: theme.mixins.toolbar,
+	drawerPaper: {
+		width: drawerWidth
+	},
+	content: {
+		flexGrow: 1,
+		padding: theme.spacing.unit * 3
+	},
+	search: {
+		position: 'relative',
+		borderRadius: theme.shape.borderRadius,
+		backgroundColor: fade(theme.palette.common.white, 0.15),
+		'&:hover': {
+			backgroundColor: fade(theme.palette.common.white, 0.25)
+		},
+		marginLeft: 0,
+		width: '100%',
+		[theme.breakpoints.up('sm')]: {
+			marginLeft: theme.spacing.unit,
+			width: 'auto'
+		}
+	},
+	searchIcon: {
+		width: theme.spacing.unit * 9,
+		height: '100%',
+		position: 'absolute',
+		pointerEvents: 'none',
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	inputRoot: {
+		color: 'inherit',
+		width: '100%'
+	},
+	inputInput: {
+		paddingTop: theme.spacing.unit,
+		paddingRight: theme.spacing.unit,
+		paddingBottom: theme.spacing.unit,
+		paddingLeft: theme.spacing.unit * 10,
+		transition: theme.transitions.create('width'),
+		width: '100%',
+		[theme.breakpoints.up('sm')]: {
+			width: 120,
+			'&:focus': {
+				width: 200
+			}
+		}
+	}
+});
+
+class ResponsiveDrawer extends React.Component {
+	state = {
+		mobileOpen: false,
+		activeDrawer: optionsTextArray[0]
+	};
+
+	handleDrawerToggle = () => {
+		this.setState(state => ({ mobileOpen: !state.mobileOpen }));
+	};
+
+	render() {
+		const { classes, theme } = this.props;
+		const { activeDrawer } = this.state;
+
+		const drawer = (
+			<div>
+				<div
+					className={classes.toolbar}
+					style={{
+						display: 'flex',
+						justifyContent: 'center',
+						alignItems: 'center'
+					}}
+				>
+					{/* Menu Heading Text*/}
+					<Typography component="h2" variant="display2">
+						Options
+					</Typography>
+				</div>
+				<Divider />
+				<List>
+					{optionsTextArray.map((text, index) => {
+						let active = activeDrawer === text ? true : false;
+						return (
+							<ListItem
+								selected={active}
+								button
+								key={index}
+								onClick={() => {
+									this.setState({ activeDrawer: text });
+									this.setState({ mobileOpen: false });
+								}}
+							>
+								<ListItemIcon>{optionsIconArray[index]}</ListItemIcon>
+								<ListItemText primary={text} />
+							</ListItem>
+						);
+					})}
+				</List>
+				{/* <Divider /> */}
+			</div>
+		);
+
+		return (
+			<div className={classes.root}>
+				<CssBaseline />
+				<AppBar position="fixed" className={classes.appBar}>
+					<Toolbar>
+						<IconButton
+							color="inherit"
+							aria-label="Open drawer"
+							onClick={this.handleDrawerToggle}
+							className={classes.menuButton}
+						>
+							<MenuIcon />
+						</IconButton>
+						{/* Show the active drawer title in the appbar */}
+						{/* <Typography variant="h6" color="inherit" noWrap>
+							{activeDrawer}
+						</Typography> */}
+						<div className={classes.grow} />
+						<div className={classes.search}>
+							<div className={classes.searchIcon}>
+								<SearchIcon />
+							</div>
+							<InputBase
+								placeholder="Search…"
+								classes={{
+									root: classes.inputRoot,
+									input: classes.inputInput
+								}}
+							/>
+						</div>
+					</Toolbar>
+				</AppBar>
+				<nav className={classes.drawer}>
+					{/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+					<Hidden smUp implementation="css">
+						<Drawer
+							container={this.props.container}
+							variant="temporary"
+							anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+							open={this.state.mobileOpen}
+							onClose={this.handleDrawerToggle}
+							classes={{
+								paper: classes.drawerPaper
+							}}
+						>
+							{drawer}
+						</Drawer>
+					</Hidden>
+					<Hidden xsDown implementation="css">
+						<Drawer
+							classes={{
+								paper: classes.drawerPaper
+							}}
+							variant="permanent"
+							open
+						>
+							{drawer}
+						</Drawer>
+					</Hidden>
+				</nav>
+				<main className={classes.content}>
+					<div className={classes.toolbar} />
+					<Typography paragraph>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+						eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
+						dolor purus non enim praesent elementum facilisis leo vel. Risus at
+						ultrices mi tempus imperdiet. Semper risus in hendrerit gravida
+						rutrum quisque non tellus. Convallis convallis tellus id interdum
+						velit laoreet id donec ultrices. Odio morbi quis commodo odio aenean
+						sed adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
+						integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
+						eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
+						quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
+						vivamus at augue. At augue eget arcu dictum varius duis at
+						consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+						donec massa sapien faucibus et molestie ac.
+					</Typography>
+					<Typography paragraph>
+						Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
+						ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
+						elementum integer enim neque volutpat ac tincidunt. Ornare
+						suspendisse sed nisi lacus sed viverra tellus. Purus sit amet
+						volutpat consequat mauris. Elementum eu facilisis sed odio morbi.
+						Euismod lacinia at quis risus sed vulputate odio. Morbi tincidunt
+						ornare massa eget egestas purus viverra accumsan in. In hendrerit
+						gravida rutrum quisque non tellus orci ac. Pellentesque nec nam
+						aliquam sem et tortor. Habitant morbi tristique senectus et.
+						Adipiscing elit duis tristique sollicitudin nibh sit. Ornare aenean
+						euismod elementum nisi quis eleifend. Commodo viverra maecenas
+						accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam
+						ultrices sagittis orci a.
+					</Typography>
+				</main>
+			</div>
+		);
+	}
+}
+
+ResponsiveDrawer.propTypes = {
+	classes: PropTypes.object.isRequired,
+	// Injected by the documentation to work in an iframe.
+	// You won't need it on your project.
+	container: PropTypes.object,
+	theme: PropTypes.object.isRequired
+};
+
+export default withStyles(styles, { withTheme: true })(ResponsiveDrawer);
